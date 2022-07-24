e94e2f146093f2c592680f7835ee4bc90d85aa23
Tiger <tigeryyang@gmail.com>
2022-07-24 17:11:06 -0500

    Add install script

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # dotfiles
 My personal dotfiles, mainly for macOs.
 
+## Installation
+Clone the repository to a directory of your choosing.
+
+```bash
+git clone https://github.com/tigeryy2/dotfiles.git
+```
+
+You will need to create symlinks between the dotfiles in this repository and your home directory.
+Otherwise, the dotfiles will not be picked up by your system.
+
+There is included an install script. This will remove the current dotfiles and replace them with symlinks.
+WARNING: This is delete any dotfiles that have the same name as those in this repo.
+
+macOs:
+```bash
+source install-scripts/osx_install.sh
+```
+
 ## Thanks
-- Many lines borrowed from [Mathias Bynens's dotfiles repository](https://github.com/mathiasbynens/dotfiles)
+- Many lines and files borrowed from [Mathias Bynens's dotfiles repository](https://github.com/mathiasbynens/dotfiles
+- Install script adapted from https://github.com/mattjmorrison/dotfiles/blob/master/install-scripts/OSX/setup.sh
---
+++ install-scripts/osx_install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#==============
+# Functions
+#==============
+# from https://stackoverflow.com/questions/3915040/how-to-obtain-the-absolute-path-of-a-file-via-shell-bash-zsh-sh
+# get absolute path from relative path
+get_abs_filename() {
+  # $1 : relative filename
+  if [ -d "$(dirname "$1")" ]; then
+    echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+  fi
+}
+
+# create symlink at home dir to file from relative path
+# converts relative path to absolute before creating the symlink
+create_symlink() {
+  # $1 : relative filename to dotfile
+  ln -s "$(get_abs_filename "$1")" ~/$1
+  echo $1
+}
+
+#==============
+# Remove old dot flies
+#==============
+# removes old dot file, redircting all stout and sterr into the void
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.gitignore > /dev/null 2>&1
+sudo rm -rf ~/.gitmessage > /dev/null 2>&1
+
+#==============
+# Create symlinks in the home folder
+#==============
+SYMLINKS=()
+SYMLINKS+=($(create_symlink ".gitconfig"))
+SYMLINKS+=($(create_symlink ".gitignore"))
+SYMLINKS+=($(create_symlink ".gitmessage"))
+
+#==============
+# Done
+#==============
+echo ${SYMLINKS[@]}
+echo -e "\n====== All Done!! ======\n"
+echo
